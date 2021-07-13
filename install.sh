d30146e64f064fc7609236189c7bd9451f500ec7
Judah Olotu <judah.olotu@shopify.com>
2021-07-13 11:14:44 -0400

    revert

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ if [[ "$SPIN" = 1 ]]
 then
   if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
   then
-    SHOP_ID=1 rake dev:orders:generate_dummy_orders
+    cartridge insert Projudah/judah-base-env
+    . /cartridges/judah-base-env/setup.sh
   fi
 fi
