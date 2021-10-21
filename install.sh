939c2699021a8e606a0020fd0e1dd271ccfa70f0
Judah Olotu <judah.olotu@shopify.com>
2021-10-21 14:10:09 -0400

    gave up

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo apt-get -y install nano
 if [[ "$SPIN" = 1 ]]
 then
-  if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
-  then
-    cartridge insert Projudah/judah-base-env
-    . /cartridges/judah-base-env/setup.sh
-  fi
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sudo apt-get -y install nano
+  cartridge insert Projudah/judah-base-env
 fi
