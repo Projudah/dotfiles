ccafe47d1d6303908066a56879481a59476b7b5d
Judah Olotu <judaholotu@Judahs-MacBook-Pro-2.local>
2022-01-14 09:44:42 -0500

    remove spin condition

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-if [[ "$SPIN" = 1 ]]
-then
-  sudo apt-get -y install nano
-  cartridge insert Projudah/judah-base-env
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
+sudo apt-get -y install nano
+cartridge insert Projudah/judah-base-env
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
